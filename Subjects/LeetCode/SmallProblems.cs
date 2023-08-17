a5fbf01b22c6feaa2b3f4008d19da41d94c38342
ncn-ends <ncn.ends@gmail.com>
2023-08-17 06:50:39 -0700

    GetConcatenation

---
+++ Subjects/LeetCode/SmallProblems.cs
@@ -0,0 +1,16 @@
+namespace Subjects.LeetCode;
+
+public class SmallProblems
+{
+    public static int[] GetConcatenation(int[] nums)
+    {
+        var arr = new int[nums.Length * 2];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            arr[i] = nums[i];
+            arr[i + nums.Length] = nums[i];
+        }
+
+        return arr;
+    }
+}
