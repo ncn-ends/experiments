fbbce45c8f9c1365cad33d8c9438835bb3f21e81
ncn-ends <ncn.ends@gmail.com>
2023-11-19 11:56:42 -0800

    2017 day 12 part 2

--- AoC/2017/12/Day12Solutions.cs
+++ AoC/2017/12/Day12Solutions.cs
@@ -21,8 +21,6 @@ public static class Day12Solutions
 """;
         var map = new AdjacencyMap();
 
-
-        /* parse into adjacency map */
         // importExample.
         AocInputHandler.ImportHttp().IterateOnEachLine((x, _) =>
         {
@@ -46,7 +44,52 @@ public static class Day12Solutions
             }
         }
 
-        Debugger.Break();
         return visited.Count;
+    }
+
+    public static int SolvePart2()
+    {
+        var importExample =
+"""
+0 <-> 2
+1 <-> 1
+2 <-> 0, 3, 4
+3 <-> 2, 4
+4 <-> 2, 3, 6
+5 <-> 6
+6 <-> 4, 5
+""";
+        var map = new AdjacencyMap();
+
+        // importExample
+        AocInputHandler.ImportHttp().IterateOnEachLine((x, _) =>
+        {
+            var nums = x.ExtractNumbers();
+            var homeNodeVal = nums[0];
+            var connections = nums.Skip(1);
+            map.AddNodeWithEdges(homeNodeVal, [..connections]);
+        });
+
+        var visited = new HashSet<AdjacencyMapNode>();
+        var queue = new Queue<AdjacencyMapNode>();
+        var groups = 0;
+
+        for (var nextNode = map.From(0);
+             nextNode != null;
+             nextNode = map.Nodes.FirstOrDefault(pair => !visited.Contains(pair.Value)).Value)
+        {
+            queue.Enqueue(nextNode);
+            while (queue.Any())
+            {
+                var current = queue.Dequeue();
+                visited.Add(current);
+                foreach (var edge in current.Edges)
+                    if (!visited.Contains(edge)) queue.Enqueue(edge);
+            }
+
+            groups++;
+        }
+
+        return groups;
     }
 }
--- App/Program.cs
+++ App/Program.cs
@@ -5,7 +5,7 @@ using System.Diagnostics;
 using AoC.Y2017;
 using static Tests.Utils.PrintUtility;
 
-var solution = Day12Solutions.SolvePart1();
+var solution = Day12Solutions.SolvePart2();
 Print(solution);
 
 // List<int> qwe = [1, 2, 3, 4];
