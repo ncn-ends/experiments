aac1793e2a3c30f4de2bd57572ed2d9f9fca01cb
ncn-ends <ncn.ends@gmail.com>
2023-11-25 19:48:07 -0800

    aoc 2016 day 6

---
+++ AoC2/2016/6/Day6Solutions.cs
@@ -0,0 +1,96 @@
+using System.Diagnostics;
+using Utils.Strings;
+
+namespace AoC._2016;
+
+public class Day6Solutions
+{
+    private static readonly string _example = """
+                                              eedadn
+                                              drvtee
+                                              eandsr
+                                              raavrd
+                                              atevrs
+                                              tsrnev
+                                              sdttsa
+                                              rasrtv
+                                              nssdts
+                                              ntnada
+                                              svetve
+                                              tesnvt
+                                              vntsnd
+                                              vrdear
+                                              dvrsen
+                                              enarar
+                                              """;
+
+    [Test]
+    public void DoPart1()
+    {
+        Assert.That(SolvePart1(_example), Is.EqualTo("easter"));
+
+        var res = SolvePart1(AocInputHandler.ImportHttp());
+        TestContext.Out.WriteLine(res);
+    }
+
+    public static string SolvePart1(string input)
+    {
+        var dict = new Dictionary<int, Dictionary<char, int>>();
+        var cols = input.SplitByLine()[0].Length;
+        for (var i = 0; i < cols; i++)
+            dict[i] = new();
+
+        input.IterateOnEachLine((line, _) =>
+        {
+            for (var cha = 0; cha < line.Length; cha++)
+            {
+                if (dict[cha].ContainsKey(line[cha])) dict[cha][line[cha]]++;
+                else
+                {
+                    dict[cha][line[cha]] = 1;
+                }
+            }
+        });
+        var msg = "";
+        foreach (var dictValue in dict.Values)
+        {
+            msg += dictValue.Aggregate((x, y) => x.Value > y.Value ? x : y).Key;
+        }
+        return msg;
+    }
+
+    [Test]
+    public static void DoPart2()
+    {
+        Assert.That(SolvePart2(_example), Is.EqualTo("advent"));
+
+        var res = SolvePart2(AocInputHandler.ImportHttp());
+        TestContext.Out.WriteLine(res);
+    }
+
+    public static string SolvePart2(string input)
+    {
+        var dict = new Dictionary<int, Dictionary<char, int>>();
+        var cols = input.SplitByLine()[0].Length;
+        for (var i = 0; i < cols; i++)
+            dict[i] = new();
+
+        input.IterateOnEachLine((line, _) =>
+        {
+            for (var cha = 0; cha < line.Length; cha++)
+            {
+                if (dict[cha].ContainsKey(line[cha])) dict[cha][line[cha]]++;
+                else
+                {
+                    dict[cha][line[cha]] = 1;
+                }
+            }
+        });
+
+        var msg = "";
+        foreach (var dictValue in dict.Values)
+            msg += dictValue.Aggregate((x, y) => x.Value < y.Value ? x : y).Key;
+
+        return msg;
+    }
+}
