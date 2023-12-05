d659623edcbe7c2dd17d8c907e9a1cced4e46c8a
ncn-ends <ncn.ends@gmail.com>
2023-12-04 19:10:26 -0800

    moved to charp directory and new structure

---
+++ csharp/Benchmarks/LeetCode/TwoSumBenchmarks.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using BenchmarkDotNet.Running;
+using Subjects.LeetCode;
+
+namespace Benchmarks.LeetCode;
+
+[MemoryDiagnoser] /* Information about memory usage */
+[Orderer(SummaryOrderPolicy.FastestToSlowest)] /* Order results by fastest to slowest, duh */
+[RankColumn]
+public class TwoSumBenchmarks
+{
+    private static readonly List<int> _sample = new() {2, 7, 11, 15};
+    private const int _target = 9;
+    private static readonly TwoSum _twoSum = new(_sample, _target);
+    
+    public static void RunBenchmarks() => 
+        BenchmarkRunner.Run<TwoSumBenchmarks>();
+
+
+    [Benchmark(Baseline = true)]
+    public void GetTwoSumSolutionA() => _twoSum.SolutionA();
+    
+    [Benchmark]
+    public void GetTwoSumSolutionB() => _twoSum.SolutionB();
+
+    [Benchmark]
+    public void GetTwoSumSolutionC() => _twoSum.SolutionC();
+
+    [Benchmark]
+    public void GetTwoSumSolutionD() => _twoSum.SolutionD();
+
+    [Benchmark]
+    public void GetTwoSumSolutionE() => _twoSum.SolutionE();
+}
---
+++ csharp/Subjects/LeetCode/TwoSum.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Diagnostics;
+
+namespace Subjects.LeetCode;
+
+/* https://leetcode.com/problems/two-sum/ */
+public class TwoSum
+{
+    private readonly List<int> _nums;
+    private readonly int _target;
+    public TwoSum(List<int> nums, int target)
+    {
+        _nums = nums;
+        _target = target;
+    }
+    
+    /*
+     * Brute force solution
+     * - O(n^2)
+     */
+    public (int, int) SolutionA()
+    {
+        int firstPart = -1;
+        int secondPart = -1;
+        
+        for (int i = 0; i < _nums.Count; i++)
+        {
+            if (i + 1 >= _nums.Count) break;
+            for (int j = i + 1; j < _nums.Count; j++)
+            {
+                if (_nums[i] + _nums[j] != _target) continue;
+                firstPart = i;
+                secondPart = j;
+            }
+        }
+
+        return (firstPart, secondPart);
+    }
+
+    public (int, int) SolutionB()
+    {
+        int firstPart = -1;
+        int secondPart = -1;
+        
+        for (int i = 0; i < _nums.Count; i++)
+        {
+            if (_nums[i] > _target) continue; 
+            if (i + 1 >= _nums.Count) break;
+            for (int j = i + 1; j < _nums.Count; j++)
+            {
+                if (_nums[i] + _nums[j] != _target) continue;
+                firstPart = i;
+                secondPart = j;
+            }
+        }
+
+        return (firstPart, secondPart);
+    }
+
+    /* --- NOT MY SOLUTION ---
+     * - even though it's O(n), requires more memory allocation and takes longer for smaller datasets
+     */
+    public (int, int)? SolutionC()
+    {
+        Dictionary<int, int> dict = new();
+        for (int i = 0; i < _nums.Count; i++)
+        {
+            dict[_nums[i]] = i;
+        }
+
+        for (int i = 0; i < _nums.Count; i++)
+        {
+            int compliment = _target - _nums[i];
+            if (dict.ContainsKey(compliment) && dict[compliment] != i)
+            {
+                return (i, dict[compliment]);
+            }
+        }
+
+        return null;
+    }
+    
+    public (int, int)? SolutionD()
+    {
+        Dictionary<int, int> dict = new();
+
+        for (int i = 0; i < _nums.Count; i++)
+        {
+            int compliment = _target - _nums[i];
+            if (dict.ContainsKey(compliment))
+            {
+                return (dict[compliment], i);
+            }
+            dict[_nums[i]] = i;
+        }
+
+        return null;
+    }
+    
+    
+    public (int, int)? SolutionE()
+    {
+        Hashtable dict = new();
+
+        for (int i = 0; i < _nums.Count; i++)
+        {
+            int compliment = _target - _nums[i];
+            if (dict.ContainsKey(compliment))
+            {
+                return ((int)dict[compliment], i);
+            }
+            dict[_nums[i]] = i;
+        }
+
+        return null;
+    }
+}
