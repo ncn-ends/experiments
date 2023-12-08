cc8d34b3cd298fdaf3eae7dcda850738c8ca8db9
ncn-ends <ncn.ends@gmail.com>
2023-12-08 08:26:03 -0800

    cleanup aoc 2023 day 8

---
+++ csharp/NUnitTests/Test_NumberHelpers.cs
@@ -0,0 +1,20 @@
+using Utils.Numbers;
+
+
+namespace NUnitTests;
+
+public class Test_NumberHelpers
+{
+    [Test]
+    public static void TestLCM()
+    {
+        List<int> case1 = [555, 444, 333];
+        Assert.That(NumberHelpers.GetLCM(case1), Is.EqualTo(6660));
+
+        List<int> case2 = [2, 3];
+        Assert.That(NumberHelpers.GetLCM(case2), Is.EqualTo(6));
+
+        List<long> case3 = [12169, 20093, 20659, 22357, 13301, 18961];
+        Assert.That(NumberHelpers.GetLCMLong(case3), Is.EqualTo(15690466351717));
+    }
+}
